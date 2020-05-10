4c37c0dfe114f44708b4eaeb737c8349514815cb
elimuraya <elimuraya.dev@gmail.com>
2020-05-10 15:19:25 +0300

    * Add token field in the configuration class to allow user to set a static token instead of generating ewith every request. * Update test cases. * Add README.md
    Signed-off-by: elimuraya <elimuraya.dev@gmail.com>

--- src/test/java/MpesaIntegrationTest.java
+++ src/test/java/MpesaIntegrationTest.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tech.bytespot.mpesa_api.configurations.MpesaConfiguration;
@@ -7,12 +9,17 @@ import tech.bytespot.mpesa_api.utils.MpesaConstants;
 import tech.bytespot.mpesa_api.utils.MpesaException;
 import tech.bytespot.mpesa_api.utils.core.Commands;
 import tech.bytespot.mpesa_api.utils.core.Identifiers;
-import tech.bytespot.mpesa_api.wrappers.user.*;
-
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
+import tech.bytespot.mpesa_api.wrappers.user.AccountBalanceRequest;
+import tech.bytespot.mpesa_api.wrappers.user.B2B_Request;
+import tech.bytespot.mpesa_api.wrappers.user.B2C_Request;
+import tech.bytespot.mpesa_api.wrappers.user.C2B_RegisterRequest;
+import tech.bytespot.mpesa_api.wrappers.user.C2B_SimulateRequest;
+import tech.bytespot.mpesa_api.wrappers.user.Reversal_Request;
+import tech.bytespot.mpesa_api.wrappers.user.STKPush_Request;
+import tech.bytespot.mpesa_api.wrappers.user.TransactionStatusRequest;
 
 public class MpesaIntegrationTest {
+
   private final static Logger LOGGER = Logger.getLogger(MpesaIntegrationTest.class.getName());
   MpesaService mpesaService = new MpesaService();
   String appKey = System.getenv("TEST_APPKEY");
@@ -35,170 +42,181 @@ public class MpesaIntegrationTest {
   String initiatorPassword = System.getenv("TEST_INITIATOR_PASSWORD");
   String securityCredential = System.getenv("TEST_SECURITY_CREDENTIAL");
   String passKey = System.getenv("TEST_PASSKEY");
+  String token = "GDs2vGAglCInBKKazV9DxqRoFAKA";
 
   MpesaConfiguration payinMpesaConfiguration = MpesaConfiguration.builder()
-          .shortcode("601468")
-          .shortcode2("600000")
-          .inAppMode(MpesaUtils.Test_Mode)
-          .withAppKey(appKey)
-          .withAppSecret(appSecret)
-          .withTestMsisdn("254708374149")
-          .enableSTK("174379",
-                  passKey,
-                  baseUrl + "stk")
-          .enableC2B(baseUrl + "validation",
-                  baseUrl + "confirmation")
-          .enableReversal(baseUrl + "reversal",
-                  baseUrl + "reversal",
-                  initiatorName,
-                  initiatorPassword,
-                  securityCredential
-          ).enableBalanceCheck(baseUrl + "balance",
-                  baseUrl + "balance",
-                  initiatorName,
-                  initiatorPassword,
-                  securityCredential
-          ).enableStatusCheck(baseUrl + "status",
-                  baseUrl + "status",
-                  initiatorName,
-                  initiatorPassword,
-                  securityCredential
-          ).setHttpTimeouts(15, 15, TimeUnit.SECONDS)
-          .build();
+      .shortcode("601468")
+      .shortcode2("600000")
+      .inAppMode(MpesaUtils.Test_Mode)
+      .withAppKey(appKey)
+      .withAppSecret(appSecret)
+      .withTestMsisdn("254708374149")
+      .setAccessToken(token)
+      .enableSTK("174379",
+          passKey,
+          baseUrl + "stk")
+      .enableC2B(baseUrl + "validation",
+          baseUrl + "confirmation")
+      .enableReversal(baseUrl + "reversal",
+          baseUrl + "reversal",
+          initiatorName,
+          initiatorPassword,
+          securityCredential
+      ).enableBalanceCheck(baseUrl + "balance",
+          baseUrl + "balance",
+          initiatorName,
+          initiatorPassword,
+          securityCredential
+      ).enableStatusCheck(baseUrl + "status",
+          baseUrl + "status",
+          initiatorName,
+          initiatorPassword,
+          securityCredential
+      ).setHttpTimeouts(15, 15, TimeUnit.SECONDS)
+      .build();
 
   MpesaConfiguration payOutMpesaConfiguration = MpesaConfiguration.builder()
-          .shortcode("601468")
-          .shortcode2("600000")
-          .inAppMode(MpesaUtils.Test_Mode)
-          .withAppKey(appKey)
-          .withAppSecret(appSecret)
-          .withTestMsisdn("254708374149")
-          .enableB2C(baseUrl + "b2c",
-                  baseUrl + "b2c",
-                  initiatorName,
-                  initiatorPassword,
-                  securityCredential
-          )
-          .enableB2B(baseUrl + "b2b",
-                  baseUrl + "b2b",
-                  initiatorName,
-                  initiatorPassword,
-                  securityCredential
-          ).enableBalanceCheck(baseUrl + "balance",
-                  baseUrl + "balance",
-                  initiatorName,
-                  initiatorPassword,
-                  securityCredential
-          ).enableStatusCheck(baseUrl + "status",
-                  baseUrl + "status",
-                  initiatorName,
-                  initiatorPassword,
-                  securityCredential
-          ).setHttpTimeouts(15, 15, TimeUnit.SECONDS)
-          .build();
+      .shortcode("601468")
+      .shortcode2("600000")
+      .inAppMode(MpesaUtils.Test_Mode)
+      .withAppKey(appKey)
+      .withAppSecret(appSecret)
+      .withTestMsisdn("254708374149")
+      .enableB2C(baseUrl + "b2c",
+          baseUrl + "b2c",
+          initiatorName,
+          initiatorPassword,
+          securityCredential
+      )
+      .enableB2B(baseUrl + "b2b",
+          baseUrl + "b2b",
+          initiatorName,
+          initiatorPassword,
+          securityCredential
+      ).enableBalanceCheck(baseUrl + "balance",
+          baseUrl + "balance",
+          initiatorName,
+          initiatorPassword,
+          securityCredential
+      ).enableStatusCheck(baseUrl + "status",
+          baseUrl + "status",
+          initiatorName,
+          initiatorPassword,
+          securityCredential
+      ).setHttpTimeouts(15, 15, TimeUnit.SECONDS)
+      .build();
 
   @Test
   void stkPushRequest() throws MpesaException {
     STKPush_Request request = STKPush_Request.builder()
-            .forPhoneNumber(testMsisdn)
-            .withAmount(testStkAmount)
-            .withDescription("test STK push")
-            .withAccountReference(testAccountRef)
-            .build();
+        .forPhoneNumber(testMsisdn)
+        .withAmount(testStkAmount)
+        .withDescription("test STK push")
+        .withAccountReference(testAccountRef)
+        .build();
     var response = mpesaService.stkPushRequest(request, payinMpesaConfiguration);
-    Assertions.assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
+    Assertions
+        .assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
   }
 
   @Test
   void stkPushQueryRequest() throws MpesaException {
     var response = mpesaService.stkQueryRequest(teststkTransactionId, payinMpesaConfiguration);
-    Assertions.assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
+    Assertions
+        .assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
   }
 
   @Test
   void c2bRegistrationRequest() throws MpesaException {
     C2B_RegisterRequest request = C2B_RegisterRequest.builder()
-            .withResponseType(MpesaConstants.ResponseType_Completed)
-            .build();
+        .withResponseType(MpesaConstants.ResponseType_Completed)
+        .build();
     var response = mpesaService.c2bRegisterRequest(request, payinMpesaConfiguration);
-    Assertions.assertTrue(response.getResponseDescription().equals("success"), response.getResponseDescription());
+    Assertions.assertTrue(response.getResponseDescription().equals("success"),
+        response.getResponseDescription());
   }
 
   @Test
   void c2bSimulation() throws MpesaException {
     C2B_SimulateRequest request = C2B_SimulateRequest.builder()
-            .forPhoneNumber(testMsisdn)
-            .withAmount(testB2BAmount)
-            .withReferenceNo(testAccountRef)
-            .withCommandId(Commands.CustomerPayBillOnline)
-            .build();
+        .forPhoneNumber(testMsisdn)
+        .withAmount(testB2BAmount)
+        .withReferenceNo(testAccountRef)
+        .withCommandId(Commands.CustomerPayBillOnline)
+        .build();
     var response = mpesaService.c2bSimulateRequest(request, payinMpesaConfiguration);
-    Assertions.assertTrue(response.getResponseDescription().startsWith("Accept"), response.getResponseDescription());
+    Assertions.assertTrue(response.getResponseDescription().startsWith("Accept"),
+        response.getResponseDescription());
   }
 
   @Test
   void reversalRequest() throws MpesaException {
     Reversal_Request request = Reversal_Request.builder()
-            .forTransactionId(testTransactionId)
-            .withAmount(testB2BAmount)
-            .withComment(testComment)
-            .withOccasion(testOccasion)
-            .build();
+        .forTransactionId(testTransactionId)
+        .withAmount(testB2BAmount)
+        .withComment(testComment)
+        .withOccasion(testOccasion)
+        .build();
     var response = mpesaService.reversalRequest(request, payinMpesaConfiguration);
-    Assertions.assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
+    Assertions
+        .assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
   }
 
   @Test
   void transactionStatusRequest() throws MpesaException {
     TransactionStatusRequest request = TransactionStatusRequest.builder()
-            .forTransactionId(testTransactionId)
-            .useIdentifierType(String.valueOf(Identifiers.SHORTCODE))
-            .withComment(testComment)
-            .withOccasion(testOccasion)
-            .build();
+        .forTransactionId(testTransactionId)
+        .useIdentifierType(String.valueOf(Identifiers.SHORTCODE))
+        .withComment(testComment)
+        .withOccasion(testOccasion)
+        .build();
     var response = mpesaService.transactionStatusRequest(request, payinMpesaConfiguration);
-    Assertions.assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
+    Assertions
+        .assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
   }
 
   @Test
   void accountBalanceRequest() throws MpesaException {
     AccountBalanceRequest request = AccountBalanceRequest.builder()
-            .withComment(testComment)
-            .withIdentifierType(Identifiers.SHORTCODE)
-            .build();
+        .withComment(testComment)
+        .withIdentifierType(Identifiers.SHORTCODE)
+        .build();
     var response = mpesaService.accountBalanceRequest(request, payinMpesaConfiguration);
-    Assertions.assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
+    Assertions
+        .assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
   }
 
   @Test
   void b2cRequest() throws MpesaException {
     B2C_Request request = B2C_Request.builder()
-            .forPhoneNumber(testMsisdn)
-            .withAmount(testB2CAmount)
-            .withCommandId(Commands.PromotionPayment)
-            .withOccasion(testOccasion)
-            .withComment(testComment)
-            .build();
+        .forPhoneNumber(testMsisdn)
+        .withAmount(testB2CAmount)
+        .withCommandId(Commands.PromotionPayment)
+        .withOccasion(testOccasion)
+        .withComment(testComment)
+        .build();
     var response = mpesaService.b2cRequest(request, payOutMpesaConfiguration);
-    Assertions.assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
+    Assertions
+        .assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
   }
 
   @Test
   void b2bRequest() throws MpesaException {
     B2B_Request request = B2B_Request.builder()
-            .sendToTillNo(testTillNo)
-            .setReceiverIdentifierType(Identifiers.SHORTCODE)
-            .setSenderIdentifierType(Identifiers.SHORTCODE)
-            .withAmount(testB2BAmount)
-            .withReferenceNo(testAccountRef)
-            .withAmount(testB2CAmount)
-            .withCommandId(Commands.BusinessPayBill)
-            .withComment(testComment)
-            .build();
+        .sendToTillNo(testTillNo)
+        .setReceiverIdentifierType(Identifiers.SHORTCODE)
+        .setSenderIdentifierType(Identifiers.SHORTCODE)
+        .withAmount(testB2BAmount)
+        .withReferenceNo(testAccountRef)
+        .withAmount(testB2CAmount)
+        .withCommandId(Commands.BusinessPayBill)
+        .withComment(testComment)
+        .build();
 
     payOutMpesaConfiguration.getB2b().setSecurityCredential(null);
     var response = mpesaService.b2bRequest(request, payOutMpesaConfiguration);
-    Assertions.assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
+    Assertions
+        .assertTrue(response.getResponseCode().equals("0"), response.getResponseDescription());
   }
 
 }
